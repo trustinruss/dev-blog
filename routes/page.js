c6d88f37b316e693c530c0d3c202c3620c1c4c6d
trustinruss <russkranz@yahoo.com>
2019-07-18 15:52:17 -0500

    indiv blog pages

--- routes/page.js
+++ routes/page.js
@@ -22,7 +22,15 @@ router.get('/', (req, res) => {
     return turbo.globalConfig(process.env.TURBO_API_KEY, process.env.TURBO_ENV)
 	})
   .then(globalConfig => {
-    data['global'] = globalConfig // populate data object with global.json data
+		data['global'] = globalConfig // populate data object with global.json data
+		
+		//fetch blog posts:
+		const ctr = new controllers.post()
+		return ctr.get({sort: 'desc'}) //limit:1
+	})
+	.then(posts => {
+		// console.log(JSON.stringify(posts))
+		data['posts'] = posts
     res.render('home', data) // render home.mustache
   })
 	.catch(err => {
@@ -31,7 +39,31 @@ router.get('/', (req, res) => {
 			message: err.message
 		})
 	})
+})
 
+router.get('/post/:slug', (req, res) => {
+	const data = {
+		cdn: CDN
+	}
+
+	const ctr = new controllers.post()
+	ctr.get({slug:req.params.slug})
+	.then(posts => {
+		if (posts.length == 0) {
+			throw new Error('Post ' + req.params.slug + ' not found')
+			return
+		}
+
+		data['post'] = posts[0]
+		// console.log(JSON.stringify(posts))
+		res.render('post', data)
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
 })
 
 module.exports = router
